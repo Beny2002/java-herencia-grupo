f777c1cd0afabd01d67b1eb443a4604c3dcbc966
ibradd720 <ibrahimadd2007@gmail.com>
2026-01-13 14:57:26 +0100

    Hecho

---
+++ persona.java
@@ -0,0 +1,35 @@
+public class Persona {
+    String nombre;
+    String dni;
+    int edad;
+    public Persona(String nombre, String dni, int edad) {
+        this.nombre = nombre;
+    }
+
+    public Persona() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
